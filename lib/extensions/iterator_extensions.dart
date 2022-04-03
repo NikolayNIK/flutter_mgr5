89befa23b35f12e1481c4773c5395333d29e2147
NikolayNIK <nikitin.nikolay@bk.ru>
2022-04-03 23:47:48 +0800

    iterator_extensions.dart: #maybeFirst, #maybeSingle

--- lib/extensions/iterator_extensions.dart
+++ lib/extensions/iterator_extensions.dart
@@ -7,3 +7,28 @@ extension IteratorWhereNotNull<T> on Iterable<T?> {
   Iterable<T> whereNotNull() =>
       where((element) => element != null).map((e) => e!);
 }
+
+extension IteratorMaybe<T> on Iterable<T> {
+  T? get maybeSingle {
+    final iterator = this.iterator;
+    if (!iterator.moveNext()) {
+      return null;
+    }
+
+    final value = iterator.current;
+    if (iterator.moveNext()) {
+      return null;
+    }
+
+    return value;
+  }
+
+  T? get maybeFirst {
+    final iterator = this.iterator;
+    if (!iterator.moveNext()) {
+      return null;
+    }
+
+    return iterator.current;
+  }
+}

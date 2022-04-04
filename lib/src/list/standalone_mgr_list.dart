b3057aec469bc0344f19d370673886b64e4cc836
nikolaynik <n.nikitin@rosinnova.com>
2022-04-04 11:17:16 +0800

    StandaloneMgrList

---
+++ lib/src/list/standalone_mgr_list.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_mgr5/mgr5.dart';
+import 'package:flutter_mgr5/mgr5_list.dart';
+
+class StandaloneMgrList extends StatefulWidget {
+  final MgrClient mgrClient;
+  final String func;
+  final Map<String, String>? params;
+
+  const StandaloneMgrList({
+    Key? key,
+    required this.mgrClient,
+    required this.func,
+    this.params,
+  }) : super(key: key);
+
+  @override
+  State<StandaloneMgrList> createState() => _StandaloneMgrListState();
+}
+
+class _StandaloneMgrListState extends State<StandaloneMgrList> {
+  late Future<void> _loadingFuture;
+
+  MgrListModel? _model;
+  MgrListController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+
+  @override
+  void didUpdateWidget(covariant StandaloneMgrList oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    _controller?.mgrClient = widget.mgrClient;
+  }
+
+  void _load() {
+    _model = null;
+    _controller?.dispose();
+    _controller = null;
+    _loadingFuture = () async {
+      final doc =
+          await widget.mgrClient.requestXmlDocument(widget.func, widget.params);
+
+      setState(() {
+        _model = MgrListModel.fromXmlDocument(doc);
+        _controller = MgrListController(
+            mgrClient: widget.mgrClient,
+            func: widget.func,
+            params: widget.params)
+          ..items.ingestXmlDocument(doc);
+      });
+    }();
+  }
+
+  @override
+  Widget build(BuildContext context) => _model == null || _controller == null
+      ? FutureBuilder(
+          builder: (context, snapshot) => snapshot.hasError
+              ? Text(snapshot.error?.toString() ?? 'unknown error')
+              : const Center(child: CircularProgressIndicator.adaptive()))
+      : MgrList(model: _model!, controller: _controller!);
+}

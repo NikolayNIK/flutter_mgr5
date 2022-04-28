8b23ca0c66963e39466bcf93c7f290758865b74a
nikolaynik <n.nikitin@rosinnova.com>
2022-04-28 11:50:57 +0800

    MgrList: Don't dispose ScrollControllers managed by MgrListController

--- lib/src/list/mgr_list.dart
+++ lib/src/list/mgr_list.dart
@@ -956,12 +956,4 @@ class _MgrListState extends State<MgrList> {
           },
         ),
       );
-
-  @override
-  void dispose() {
-    super.dispose();
-
-    _verticalScrollController.dispose();
-    _horizontalScrollController.dispose();
-  }
 }

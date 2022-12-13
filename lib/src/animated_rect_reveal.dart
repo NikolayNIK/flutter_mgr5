bb42875660797e5e6a114d6d107cd905a42a1cd7
nikolaynik <n.nikitin@rosinnova.com>
2022-12-13 18:23:26 +0800

    AnimatedRectReveal

---
+++ lib/src/animated_rect_reveal.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/widgets.dart';
+
+Widget _clipContainerBuilder(BuildContext context, Widget? child) =>
+    ClipRect(child: child);
+
+class AnimatedRectReveal extends StatelessWidget {
+  final Widget child;
+  final TransitionBuilder containerBuilder;
+  final Rect originBox, destinationBox;
+  final Animation<double> animation;
+
+  const AnimatedRectReveal({
+    super.key,
+    required this.animation,
+    required this.originBox,
+    required this.destinationBox,
+    this.containerBuilder = _clipContainerBuilder,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final leftDiff = destinationBox.left - originBox.left;
+    final topDiff = destinationBox.top - originBox.top;
+    final widthDiff = destinationBox.width - originBox.width;
+    final heightDiff = destinationBox.height - originBox.height;
+
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, _) {
+        final value = animation.value;
+        final valueInverted = 1 - value;
+
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned(
+              left: originBox.left + leftDiff * value,
+              top: originBox.top + topDiff * value,
+              width: originBox.width + widthDiff * value,
+              height: originBox.height + heightDiff * value,
+              child: Opacity(
+                opacity: value,
+                child: containerBuilder(
+                  context,
+                  OverflowBox(
+                    minWidth: destinationBox.width,
+                    maxWidth: destinationBox.width,
+                    minHeight: destinationBox.height,
+                    maxHeight: destinationBox.height,
+                    alignment: Alignment.topLeft,
+                    child: Transform.translate(
+                      offset: Offset(
+                        leftDiff * valueInverted,
+                        topDiff * valueInverted,
+                      ),
+                      child: RepaintBoundary(
+                        child: child,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
